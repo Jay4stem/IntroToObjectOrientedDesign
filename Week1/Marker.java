d6524302bbbed3bf88405066b81a7983deffa9d4
Barbara Compagnoni <compagnb@newschool.edu>
2017-07-08 14:01:27 -0400

    Add files via upload

---
+++ Week1/Marker.java
@@ -0,0 +1,21 @@
+/**
+ * Week 1: Marker Class
+ * version 1
+ */
+
+
+public class Marker{ // this line tells the computer hey! we are making a public class called HelloWorld
+
+	String color;
+	int size;
+	int shape;
+
+	void write (){
+		//insert code to write
+	}
+
+	void draw (){
+		//insert code to write
+	}
+    
+}
